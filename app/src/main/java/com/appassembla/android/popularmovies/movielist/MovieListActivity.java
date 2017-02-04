4affe8522ebf0ceec1297b0ab821997f73605b90
Richard Thompson <richarth@gmail.com>
2017-02-04 18:30:41 +0000

    Removed unused imports

--- app/src/main/java/com/appassembla/android/popularmovies/movielist/MovieListActivity.java
+++ app/src/main/java/com/appassembla/android/popularmovies/movielist/MovieListActivity.java
@@ -1,14 +1,11 @@
 package com.appassembla.android.popularmovies.movielist;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
