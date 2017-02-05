dcab90b157b046c7fe37a3ce82c9c16b956c719f
Richard Thompson <richarth@gmail.com>
2017-02-05 12:30:21 +0000

    Added remaining movie fields

--- app/src/main/java/com/appassembla/android/popularmovies/data/Movie.java
+++ app/src/main/java/com/appassembla/android/popularmovies/data/Movie.java
@@ -4,6 +4,8 @@ import android.support.annotation.NonNull;
 
 import com.google.auto.value.AutoValue;
 
+import java.util.Date;
+
 /**
  * Created by Richard Thompson on 04/02/2017.
  */
@@ -13,8 +15,11 @@ public abstract class Movie {
     public abstract int movieId();
     public abstract String movieName();
     public abstract String posterUrl();
+    public abstract String plotSynopsis();
+    public abstract float averageRating();
+    public abstract Date releaseDate();
 
-    public static Movie create(int movieId, String movieName, String posterUrl) {
-        return new AutoValue_Movie(movieId, movieName, posterUrl);
+    public static Movie create(int movieId, String movieName, String posterUrl, String plotSynopsis, float averageRating, Date releaseDate) {
+        return new AutoValue_Movie(movieId, movieName, posterUrl, plotSynopsis, averageRating, releaseDate);
     }
 }
--- app/src/main/java/com/appassembla/android/popularmovies/data/StaticMoviesRepository.java
+++ app/src/main/java/com/appassembla/android/popularmovies/data/StaticMoviesRepository.java
@@ -3,6 +3,7 @@ package com.appassembla.android.popularmovies.data;
 import android.support.annotation.NonNull;
 
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -14,11 +15,11 @@ public class StaticMoviesRepository implements MoviesRepository {
     private List<Movie> movies;
 
     public StaticMoviesRepository() {
-        Movie movie1 = Movie.create(1, "Movie 1", "");
+        Movie movie1 = Movie.create(1, "Movie 1", "", "", 0, new Date());
 
-        Movie movie2 = Movie.create(2, "Movie 2", "");
+        Movie movie2 = Movie.create(2, "Movie 2", "", "", 0, new Date());
 
-        Movie movie3 = Movie.create(3, "Movie 3", "");
+        Movie movie3 = Movie.create(3, "Movie 3", "", "", 0, new Date());
 
         movies = Arrays.asList(movie1, movie2, movie3);
     }
