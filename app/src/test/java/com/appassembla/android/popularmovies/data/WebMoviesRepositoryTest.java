8f526b8263790596f8efac563d304637bac277a5
Richard Thompson <richarth@gmail.com>
2017-02-09 22:29:51 +0000

    Now that we have Moshi working we don't need the Movie2 POJO or test for comparison

--- app/src/test/java/com/appassembla/android/popularmovies/data/WebMoviesRepositoryTest.java
+++ app/src/test/java/com/appassembla/android/popularmovies/data/WebMoviesRepositoryTest.java
@@ -41,31 +41,4 @@ public class WebMoviesRepositoryTest {
 
         assertEquals(adapter.fromJson(sampleMovieJson), sampleMovie);
     }
-
-    @Test
-    public void verifyJsonConvertsToModel2() throws Exception {
-        String sampleMovieJson = "{\"adult\":false,\"backdrop_path\":\"/lubzBMQLLmG88CLQ4F3TxZr2Q7N.jpg\",\"belongs_to_collection\":{\"id\":427084,\"name\":\"The Secret Life of Pets Collection\",\"poster_path\":\"/aDNbXvuRiuYxk8qCwXNQQ7UEHau.jpg\",\"backdrop_path\":null},\"budget\":75000000,\"genres\":[{\"id\":12,\"name\":\"Adventure\"},{\"id\":16,\"name\":\"Animation\"},{\"id\":35,\"name\":\"Comedy\"},{\"id\":10751,\"name\":\"Family\"}],\"homepage\":\"http://www.thesecretlifeofpets.com/\",\"id\":328111,\"imdb_id\":\"tt2709768\",\"original_language\":\"en\",\"original_title\":\"The Secret Life of Pets\",\"overview\":\"The quiet life of a terrier named Max is upended when his owner takes in Duke, a stray whom Max instantly dislikes.\",\"popularity\":129.924005,\"poster_path\":\"/WLQN5aiQG8wc9SeKwixW7pAR8K.jpg\",\"production_companies\":[{\"name\":\"Universal Pictures\",\"id\":33},{\"name\":\"Dentsu\",\"id\":6452},{\"name\":\"Illumination Entertainment\",\"id\":6704}],\"production_countries\":[{\"iso_3166_1\":\"US\",\"name\":\"United States of America\"}],\"release_date\":\"2016-06-18\",\"revenue\":874333497,\"runtime\":87,\"spoken_languages\":[{\"iso_639_1\":\"en\",\"name\":\"English\"}],\"status\":\"Released\",\"tagline\":\"Think this is what they do all day?\",\"title\":\"The Secret Life of Pets\",\"video\":false,\"vote_average\":5.8,\"vote_count\":2078}";
-
-        Movie2 sampleMovie = new Movie2();
-
-        sampleMovie.setId(328111);
-        sampleMovie.setName("The Secret Life of Pets");
-        sampleMovie.setPosterUrl("/WLQN5aiQG8wc9SeKwixW7pAR8K.jpg");
-        sampleMovie.setPlotSynopsis("The quiet life of a terrier named Max is upended when his owner takes in Duke, a stray whom Max instantly dislikes.");
-        sampleMovie.setAverageRating(5.8);
-        sampleMovie.setReleaseDate("2016-06-18");
-
-        Moshi moshi = new Moshi.Builder().build();
-
-        JsonAdapter<Movie2> adapter = moshi.adapter(Movie2.class).lenient();
-
-        Movie2 convertedMovie = adapter.fromJson(sampleMovieJson);
-
-        assertEquals(sampleMovie.getId(), convertedMovie.getId());
-        assertEquals(sampleMovie.getName(), convertedMovie.getName());
-        assertEquals(sampleMovie.getPosterUrl(), convertedMovie.getPosterUrl());
-        assertEquals(sampleMovie.getPlotSynopsis(), convertedMovie.getPlotSynopsis());
-        assertEquals(sampleMovie.getAverageRating(), convertedMovie.getAverageRating(), 0);
-        assertEquals(sampleMovie.getReleaseDate(), convertedMovie.getReleaseDate());
-    }
 }
