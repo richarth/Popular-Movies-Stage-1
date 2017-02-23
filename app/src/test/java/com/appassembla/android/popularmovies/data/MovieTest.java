4ad8087341ac147ea3e732c9f643194aba668521
Richard Thompson <richarth@gmail.com>
2017-02-23 00:13:56 +0000

    Added test runner config

--- app/src/test/java/com/appassembla/android/popularmovies/data/MovieTest.java
+++ app/src/test/java/com/appassembla/android/popularmovies/data/MovieTest.java
@@ -1,12 +1,15 @@
 package com.appassembla.android.popularmovies.data;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 
 import static org.junit.Assert.*;
 
 /**
  * Created by richardthompson on 19/02/2017.
  */
+@RunWith(JUnit4.class)
 public class MovieTest {
 
     @Test
