c75c766c1242dc200d98221994247f5f081fa38b
Richard Thompson <richarth@gmail.com>
2017-02-24 09:34:05 +0000

    Now checking whether the support action bar is null before trying to get a context from it.  If we don't get a valid context then the spinner won't colour correctly so don't add the spinner

--- app/src/main/java/com/appassembla/android/popularmovies/movielist/MovieListActivity.java
+++ app/src/main/java/com/appassembla/android/popularmovies/movielist/MovieListActivity.java
@@ -13,7 +13,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -52,7 +51,6 @@ import static android.view.View.*;
  */
 public class MovieListActivity extends AppCompatActivity implements MovieListView, AdapterView.OnItemSelectedListener {
 
-    private static final String TAG = "MovieListActivity";
     /**
      * Whether or not the activity is in two-pane mode, i.e. running on a tablet
      * device.
@@ -149,12 +147,11 @@ public class MovieListActivity extends AppCompatActivity implements MovieListVie
         MenuItem item = menu.findItem(R.id.sort_order_spinner);
         sortSpinner = (Spinner) MenuItemCompat.getActionView(item);
 
-        try {
-
+        if (getSupportActionBar() != null) {
             Context themedContext = getSupportActionBar().getThemedContext();
 
             ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(themedContext,
-                        R.array.sort_order_array, android.R.layout.simple_spinner_item);
+                    R.array.sort_order_array, android.R.layout.simple_spinner_item);
             adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
             sortSpinner.setAdapter(adapter);
@@ -162,8 +159,6 @@ public class MovieListActivity extends AppCompatActivity implements MovieListVie
             sortSpinner.setSelection(lastSelectedSpinnerPosition);
 
             sortSpinner.setOnItemSelectedListener(this);
-        } catch (NullPointerException e) {
-            Log.d(TAG, e.getMessage());
         }
     }
 
